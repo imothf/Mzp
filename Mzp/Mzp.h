3504ace592773facb75dc29c4008ff5923f05aae
Dmitry Dovgoshliubnyi <darthdimon@googlemail.com>
2017-04-03 11:02:55 +0100

    Initial Commit

---
+++ Mzp/Mzp.h
@@ -0,0 +1,19 @@
+//
+//  Mzp.h
+//  Mzp
+//
+//  Created by Dmytro Dovgoshliubnyi on 03/04/2017.
+//  Copyright © 2017 Dmytro Dovgoshliubnyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Mzp.
+FOUNDATION_EXPORT double MzpVersionNumber;
+
+//! Project version string for Mzp.
+FOUNDATION_EXPORT const unsigned char MzpVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mzp/PublicHeader.h>
+
+
