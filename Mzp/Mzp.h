0745a39ff9e395dfd62a23615bfd24d93db5fc6f
Dmitry Dovgoshliubnyi <darthdimon@googlemail.com>
2017-04-03 12:01:10 +0100

    Initial commit

--- Mzp/Mzp.h
+++ Mzp/Mzp.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double MzpVersionNumber;
 FOUNDATION_EXPORT const unsigned char MzpVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Mzp/PublicHeader.h>
-
-
---
+++ Package.swift
@@ -0,0 +1,15 @@
+//
+//  Package.swift
+//  Mzp
+//
+//  Created by Dmytro Dovgoshliubnyi on 03/04/2017.
+//  Copyright © 2017 Dmytro Dovgoshliubnyi. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "Mzp",
+    dependencies: [
+    ]
+)
